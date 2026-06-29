4a4551207a3236796b39bd34759ee51232574465
AhmedElabrass25 <ahmedelabrass25@gmail.com>
2026-06-29 10:27:22 +0200

    feat:make AboutContent & AboutCard & QuestionCard in about page

--- app/(main)/about/page.tsx
+++ app/(main)/about/page.tsx
@@ -1,7 +1,24 @@
+import AboutContent from "@/features/about/AboutContent";
+import AuthorCard from "@/features/about/AuthorCard";
+import FAQSection from "@/features/about/FAQSection";
 import React from "react";
 
 const page = () => {
-  return <div>page</div>;
+  return (
+    <section className="py-24">
+      <div className="container">
+        <div className="grid gap-16 lg:grid-cols-[1fr_380px]">
+          <main className="order-2 space-y-16 lg:order-1">
+            <AboutContent />
+            <FAQSection />
+          </main>
+          <aside className="order-1 lg:order-2">
+            <AuthorCard />
+          </aside>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default page;
